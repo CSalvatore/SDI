c877e440a7a781252fc29e44d7e671652337b9f9
Cory Salvatore <Coryjg@Corys-MacBook-Pro.local>
2014-11-08 17:22:18 -0500

    Lab 6 for SDI 3

--- sdi-lab6/Salvatore_Cory_lab6arrays.js
+++ sdi-lab6/Salvatore_Cory_lab6arrays.js
@@ -6,8 +6,16 @@
 
 var arrayFunction = function()
 {
-	var familyArray = ["John Salvatore", "Gladys Salvatore", "Jason Salvatore", "John Salvatore"];
+	var familyArray = ["John Salvatore", "Gladys Salvatore", "Jason Salvatore", "John-John Salvatore"];
 	var relationArray = ["Father", "Mother", "Eldest Half-Brother", "Middle Half-Brother"];
+	
+	for(var i = 0; i < familyArray.length; i++)
+	{
+		console.log(familyArray[i] + " is my " + relationArray[i] + ".\n");
+	}
+	
+	familyArray.push("Michelle");
+	relationArray.push("Sister-in-Law");
 	
 	for(var i = 0; i < familyArray.length; i++)
 	{
