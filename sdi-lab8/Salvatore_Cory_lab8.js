0f5b465357d300dac3fc4b51b1e58588f5a57598
Cory Salvatore <Coryjg@Corys-MacBook-Pro.local>
2014-11-13 17:52:37 -0500

    Lab 8 for SDI 3

--- sdi-lab8/Salvatore_Cory_lab8.js
+++ sdi-lab8/Salvatore_Cory_lab8.js
@@ -4,51 +4,63 @@
 // SDI 1411
 // Lab 8 Strings
 
-var emailCheck = function()
+// var emailCheck = function()
+// {
+	// var userEmail 	  = prompt("Enter your email address.");
+	// var hasAtSign	  = false;
+	// var hasPeriod  	  = false;
+	// var hasSpace      = false;
+	// var atCounter 	  = 0;
+	// var periodCounter = 0;
+	// var atIndex;
+	// var periodIndex;
+// 	
+	// for (var i=0; i < userEmail.length; i++) 
+	// {
+		// if(atCounter <= 1)
+		// {
+	    	// if(userEmail[i] == "@")
+	 		// {
+	  			// hasAtSign = true;
+	  			// atCounter++;
+	  			// atIndex = userEmail[i].indexOf();
+	 	 	// }
+	 	// }
+	 	// else
+	 	// {
+	 		// hasAtSign = false;
+	 	// }
+	 	// if(periodCounter <= 1)
+	    	// if(userEmail[i] = ".")
+	 		// {
+	  			// hasPeriod = true;
+	  			// periodCounter++;
+	  			// periodIndex = userEmail[i].indexOf();
+	  		// }
+	  	// else
+	 	// {
+	 		// hasPeriod = false;
+	 	// }
+	 	// if(userEmail[i] == " ")
+	 	// {
+	 		// hasSpace = true;
+	 	// }
+	// };
+	// if(hasAtSign && hasPeriod && atIndex < periodIndex && !hasSpace)
+	  // {
+	  	// console.log("The email you have entered is " + userEmail + ".");
+	  // }
+	  // else
+	  // {
+	  	// console.log(userEmail + " is not a valid email address.");
+	  // }
+// };
+// 
+// emailCheck();
+
+var threeStrings = function(userString, firstSep, secondSep)
 {
-	var userEmail = prompt("Enter your email address.");
-	var hasAtSign = false;
-	var hasPeriod = false;
-	var atCounter = 0;
-	var periodCounter = 0;
-	var atIndex;
-	var periodIndex;
 	
-	for (var i=0; i < userEmail.length; i++) 
-	{
-		if(atCounter <= 1)
-		{
-	    	if(userEmail[i] == "@")
-	 		{
-	  			hasAtSign = true;
-	  			atCounter++;
-	  			atIndex = userEmail[i].indexOf();
-	 	 	}
-	 	}
-	 	else
-	 	{
-	 		hasAtSign = false;
-	 	}
-	 	if(periodCounter <= 1)
-	    	if(userEmail[i] = ".")
-	 		{
-	  			hasPeriod = true;
-	  			periodCounter++;
-	  			periodIndex = userEmail[i].indexOf();
-	  		}
-	  	else
-	 	{
-	 		hasPeriod = false;
-	 	}
-	};
-	if(hasAtSign && hasPeriod && atIndex < periodIndex)
-	  {
-	  	console.log("The email you have entered is " + userEmail + ".");
-	  }
-	  else
-	  {
-	  	console.log(userEmail + " is not a valid email address.");
-	  }
 };
 
-emailCheck();
+threeStrings(prompt("Enter a string"), prompt("Enter a separator"), prompt("Enter a new seperator"));
