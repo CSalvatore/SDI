03f533bbd3bbe58efca83d330aa37db6e605c918
Cory Salvatore <Coryjg@Corys-MacBook-Pro.local>
2014-11-13 17:39:04 -0500

    Lab 8 for SDI 2

--- sdi-lab8/Salvatore_Cory_lab8.js
+++ sdi-lab8/Salvatore_Cory_lab8.js
@@ -6,7 +6,49 @@
 
 var emailCheck = function()
 {
+	var userEmail = prompt("Enter your email address.");
+	var hasAtSign = false;
+	var hasPeriod = false;
+	var atCounter = 0;
+	var periodCounter = 0;
+	var atIndex;
+	var periodIndex;
 	
+	for (var i=0; i < userEmail.length; i++) 
+	{
+		if(atCounter <= 1)
+		{
+	    	if(userEmail[i] == "@")
+	 		{
+	  			hasAtSign = true;
+	  			atCounter++;
+	  			atIndex = userEmail[i].indexOf();
+	 	 	}
+	 	}
+	 	else
+	 	{
+	 		hasAtSign = false;
+	 	}
+	 	if(periodCounter <= 1)
+	    	if(userEmail[i] = ".")
+	 		{
+	  			hasPeriod = true;
+	  			periodCounter++;
+	  			periodIndex = userEmail[i].indexOf();
+	  		}
+	  	else
+	 	{
+	 		hasPeriod = false;
+	 	}
+	};
+	if(hasAtSign && hasPeriod && atIndex < periodIndex)
+	  {
+	  	console.log("The email you have entered is " + userEmail + ".");
+	  }
+	  else
+	  {
+	  	console.log(userEmail + " is not a valid email address.");
+	  }
 };
 
 emailCheck();
